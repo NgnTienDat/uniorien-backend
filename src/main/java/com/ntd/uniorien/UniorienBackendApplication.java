6393025fc55d981e70f228e944e89e94cfb801bd
Nguyen Tien Dat <tie.dat2004@gmail.com>
2025-10-04 11:24:51 +0700

    Initial commit

---
+++ src/main/java/com/ntd/uniorien/UniorienBackendApplication.java
@@ -0,0 +1,16 @@
+package com.ntd.uniorien;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniorienBackendApplication {
+
+	public static void main(String[] args) {
+//		Dotenv dotenv = Dotenv.load();
+//		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		SpringApplication.run(UniorienBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ntd/uniorien/UniorienBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntd.uniorien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniorienBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
